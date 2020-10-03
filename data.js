1516ca1bf868b6ea3e3b627ccc68fda9407f513c
Manny <614259041@webmail.npru.ac.th>
2020-10-03 19:54:46 +0700

    first commit food

---
+++ data.js
@@ -0,0 +1,64 @@
+const data = [
+    { 
+       id : 1,
+       name : "McDonald's (แมคโดนัลด์) - พาต้า ปิ่นเกล้า",
+       type : "ฟาสต์ฟู้ด",
+       imgUrl: "https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/THGFIST000004bl/hero/5c2f639864be46968813496061ab209c_1600008272596123546.jpeg"
+        },
+        {
+            id : 2,
+            name : "KHIANG (เขียง) - ถนนพระอาทิตย์",
+            type : "Partner,ข้าวหน้า",
+            imgUrl: "https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CZLGGETXRBV3A6/hero/253e8b332b804905b721ddf999d8d871_1594631339423966806.jpeg"
+        },
+        {
+            id : 3,
+            name : "Jones' Salad -โรงพยาบาลศิริราช",
+            type : "Coupon,Partner,อาหารสุขภาพ",
+            imgUrl: "https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CYTEEN2TPCNACT/hero/9e867c07c59a499fad5d5c2bd00d3ac5_1598195187208581539.jpg"
+        },
+        {
+            id : 4,
+            name : "ไก่ไม่ยอมนอน - ซอยรามบุตรี",
+            type : "Coupon,Partner,ข้าวหน้า",
+            imgUrl: "https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CZLDNAATVA2UNJ/hero/cab978976ec14ec3bffc196e49fcd919_1594554201623013383.png"
+        },
+        {
+            id : 5,
+            name : "KFC (เคเอฟซี) - ดิโอลด์สยาม พลาซ่า",
+            type : "Partner,ฟาสต์ฟู้ด,ไก่ทอด",
+            imgUrl: "https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/THGFIST000007ad/hero/e2884691062d4d678794f738484060d7_1600007790046638981.jpeg"
+        },
+        {
+            id : 6,
+            name : "Pizza Hut (พิซซ่าฮัท) - ถนนบำรุงเมือง",
+            type : "Coupon,Partner,พิซซ่า",
+            imgUrl: "https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CYTDJJE1VY2BEJ/hero/1d6f0d857f8e45288f5f646ba3817bb7_1598195166217373044.jpg"
+        },
+        {
+            id : 7,
+            name : "น้ำปั่นและอาหารตามสั่ง - วังหลัง",
+            type : "Coupon,Partner,อาหารตามสั่ง",
+            imgUrl: "https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CZDZFAK1A7JYCA/hero/a5789cc33e2e4767badb66b43e5c9da9_1587974688411667662.jpeg"
+        },
+        {
+            id : 8,
+            name : "อ้วนต้วน ตามสั่ง - ถนนพระราม 4",
+            type : "Coupon,Partner,อาหารตามสั่ง",
+            imgUrl: "https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CZECGEEVL63XRE/hero/5ff15d562a2f4a2c9a3d88f1e4ee0e00_1596528753489121273.jpeg"
+        },
+        {
+            id : 9,
+            name : "บุงกี่ เป็ดย่างฮ่องกง - บางลำพู",
+            type : "Coupon,Partner,อาหารจีน",
+            imgUrl: "https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CY4CJ2WAKAAZFE/hero/eac950d310ee457685bb98acf537d7ac_1576573337301905327.jpeg"
+        },
+        {
+            id : 10,
+            name : "ก๋วยเตี๋ยว บะหมี่เกี๊ยว ซุปเปอร์เล้ง - ถนนอรุณอัมรินทร์",
+            type : "Coupon, Partner, อาหารเส้น",
+            imgUrl: "https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CZMHCFKFCFNYLN/hero/71ef5cb4f9a148c8912314bc70f1d84b_1597028167020573000.jpeg"
+        }
+
+];
+module.exports = data;
