2ce808e3315ae2d43e1141b8cdd3866f6c46ebca
Tanachart Chaonafai <614259044@webmail.npru.ac.th>
2020-10-08 11:39:53 +0700

    Update

--- data.js
+++ data.js
@@ -1,64 +1,64 @@
 const data = [
     {
-        id : 1,
-        name : "McDonald's (แมคโดนัลด์) - พาต้า ปิ่นเกล้า",
-        type : "ร้านอาหาร",
-        imageURL : "https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/THGFIST000004bl/hero/5c2f639864be46968813496061ab209c_1600008272596123546.jpeg"
+        id: 1,
+        name: "จิวโจ๊กบางลำพู สุกี้กะทะร้อน - ถนนตานี",
+        type: "ร้านอาหารเส้น",
+        imageURL: "https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/THGFIST000001zo/hero/upload-photo-Hero_Photo_cb41e8148e50410a8f33632e0c336c02_1552533003406029094.jpeg"
     },
     {
-        id : 2,
-        name : "Cafe Amazon (คาเฟ่ อเมซอน) - อาคารพาณิชย์ปิ่นเกล้า",
-        type : "ร้านกาแฟ",
-        imageURL : "https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CZKKA2XXFGABCX/hero/1769c1934ab141c1b78c574304d2610e_1593409065311657054.jpeg"
+        id: 2,
+        name: "ไก่ไม่ยอมนอน - ซอยรามบุตรี",
+        type: "ร้านข้าวหน้า",
+        imageURL: "https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CZLDNAATVA2UNJ/hero/cab978976ec14ec3bffc196e49fcd919_1594554201623013383.png"
     },
     {
-        id : 3,
-        name : "นายอ้วนเย็นตาโฟเสาชิงช้า - ถนนตะนาว",
-        type : "ร้านอาหารเส้น",
-        imageURL : "https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/THGFIST0000022y/hero/c4b9c9ca49f84274861dc367529d3cc0_1592216282560213295.jpeg"
+        id: 3,
+        name: "นายอ้วนเย็นตาโฟเสาชิงช้า - ถนนตะนาว",
+        type: "ร้านอาหารเส้น",
+        imageURL: "https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/THGFIST0000022y/hero/c4b9c9ca49f84274861dc367529d3cc0_1592216282560213295.jpeg"
     },
     {
-        id : 4,
-        name : "ราดหน้าเฮงยอดผัก - เยาวราช",
-        type : "ร้านอาหารเส้น",
-        imageURL : "https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/THGFIST0000002d/hero/dca01259d4e540fcbf6c7e398312737f_1589724061440001768.jpeg"
-    },  
-     {
-        id : 5,
-        name : "Passion Food อาหารคลีนเดลิเวอรี่ - สุขุมวิท",
-        type : "ร้านอาหารสุขภาพ",
-        imageURL : "https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CZCEL7JKPAWGTT/hero/4e69a04f39db436b99498b5929cf0984_1596162680320444445.jpeg"
-    },    
+        id: 4,
+        name: "Kamu Tea (คามุ ที) - แอม ไชน่าทาวน์",
+        type: "ร้านชานมไข่มุก",
+        imageURL: "https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CZAENBUFV63ZBE/hero/dd294a57c8b04d3f83429be4c437fec5_1580462688816273956.jpeg"
+    },
     {
-        id : 6,
-        name : "ข้าวมันไก่ป้าจ๋า 23 ชั่วโมง - ถนนจันทรคามพิทักษ์",
-        type : "ร้านอาหารตามสั่ง",
-        imageURL : "https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CZE1VBLYNJMJNN/hero/3a6cdd30d6594ffaabe2b2ac7eee93a1_1589790546369189511.jpeg"
-    },   
-     {
-        id : 7,
-        name : "Kamu Tea (คามุ ที) - ถนนจันทรคามพิทักษ์",
-        type : "ร้านเครื่องดื่ม",
-        imageURL : "https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CZABBF3KDA3YAT/hero/cd21c61c294c45eaae957d631f4f3bdd_1580109720841697666.jpeg"
-    },    
+        id: 5,
+        name: "Passion Food อาหารคลีนเดลิเวอรี่ - สุขุมวิท",
+        type: "ร้านอาหารสุขภาพ",
+        imageURL: "https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CZCEL7JKPAWGTT/hero/4e69a04f39db436b99498b5929cf0984_1596162680320444445.jpeg"
+    },
     {
-        id : 8,
-        name : "พอดีคำ เมี่ยงหมิวสลัดโรล - ถนนทรงพล",
-        type : "ร้านอาหารสุขภาพ",
-        imageURL : "https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CZNDLNJEG6MZJ6/hero/1ad1d01ec62b42d28e6b80a1873235a2_1598680317338129190.jpeg"
-    },  
+        id: 6,
+        name: "แพร่งนมสด - เสาชิงช้า",
+        type: "ร้านกาแฟ/ชา",
+        imageURL: "https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/THGFIST0000021s/hero/upload-photo-Hero_Photo_12e479b8999044a183e5f9660db198b0_1549616241374620352.jpeg"
+    },
     {
-        id : 9,
-        name : "Toba Cafe - ลำพยา",
-        type : "อาหารตามสั่ง",
-        imageURL : "https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CZJ1RXKEEVLKJN/hero/0b1bf7d2ce6044c3a23e4fd3ccd4669d_1600006273618721955.jpeg"
-    },  
-     
+        id: 7,
+        name: "Pad Thai Ekamai (ผัดไทเอกมัย) - พระนคร(ข้าวสาร)",
+        type: "ร้านผัดไทย",
+        imageURL: "https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CY4JLYDCVFCWHE/hero/51ca1ddc3f504270bd9a63b3cb65877b_1596379557902379882.jpg"
+    },
     {
-        id : 10,
-        name : "วอฟเฟิลฮ่องกงmrplan - ถนนเพชรเกษม",
-        type : "ขนมขบเคี้ยว",
-        imageURL : "https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CZDHREVVCCNBGA/hero/112058f4de0b46089a28c8a60ccf99c1_1588662677388124709.jpeg"
+        id: 8,
+        name: "พอดีคำ เมี่ยงหมิวสลัดโรล - ถนนทรงพล",
+        type: "ร้านอาหารสุขภาพ",
+        imageURL: "https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CZNDLNJEG6MZJ6/hero/1ad1d01ec62b42d28e6b80a1873235a2_1598680317338129190.jpeg"
+    },
+    {
+        id: 9,
+        name: "Pizza Hut (พิซซ่าฮัท) - ถนนบำรุงเมือง",
+        type: "ร้านพิซซ่า",
+        imageURL: "https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CYTDJJE1VY2BEJ/hero/132234b8dcfc4fc7aee69a0ffdf08f80_1600675134589446721.jpg"
+    },
+
+    {
+        id: 10,
+        name: "Indian Kebab & Curries - ถนนพระอาทิตย์",
+        type: "ร้านอาหารตามสั่ง",
+        imageURL: "https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CZEUVJWBCK4GVJ/hero/26f62a787d2f426c96d3acbc012b8f75_1590218946857198659.jpeg"
     }
-    ]
+]
 module.exports = data;
